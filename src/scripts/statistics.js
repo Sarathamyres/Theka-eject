db07074c23b2e335daf76580769abd7301e93bb2
Sarathamyres <thamyressara071@gmail.com>
2026-05-26 12:28:23 -0300

    feat: Aplicação de API das estatísticas

---
+++ src/scripts/statistics.js
@@ -0,0 +1,12 @@
+fetch(
+  "https://thekaapideploy2.pythonanywhere.com/institucional/estatisticas-biblioteca/",
+)
+  .then((resposta) => resposta.json())
+  .then((json) => {
+    console.log(json);
+    const data = json[0];
+
+    document.getElementById("users-percentage").innerText = data.total_usuarios;
+    document.getElementById("catalog-materials").innerText = data.total_livros;
+    document.getElementById("collection-percentage").innerText = data.total_categorias;
+  });
