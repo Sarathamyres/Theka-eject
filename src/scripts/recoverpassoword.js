79668e83b60528c5269d9ce78218ec07d0a4f3db
Sarathamyres <thamyressara071@gmail.com>
2026-05-27 13:37:30 -0300

    feat: Página js para programação do página de usuário para recuperação de senha

---
+++ src/scripts/recoverpassoword.js
@@ -0,0 +1,37 @@
+emailForRecoverPassword.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let formOk = true;
+
+  const inputEmail = emailForRecoverPassword.querySelector("input");
+  if (inputEmail.value.trim() === "") {
+    formOk = false;
+    inputEmpty(inputEmail);
+  } else {
+    inputEmail.style.border = "2px solid #f78520";
+  }
+  if (formOk) {
+    const email = new FormData(emailForRecoverPassword);
+    const dataEmail = Object.fromEntries(email.entries());
+    fetch("https://thekaapideploy2.pythonanywhere.com/auth/password/reset/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataEmail),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Erro ao solicitar recuperação de senha");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        alert("Email de recuperação enviado com sucesso!");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(
+          "Falha ao solicitar recuperação de senha. Verifique os dados ou tente novamente mais tarde.",
+        );
+      });
+  }
+});
