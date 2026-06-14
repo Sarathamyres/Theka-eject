44c2f923511cb867148a98facb4ade89edac4c77
Sarathamyres <thamyressara071@gmail.com>
2026-06-14 15:16:21 -0300

    feat: adição de imagems ao acervo via API

--- src/scripts/collectionBooks.js
+++ src/scripts/collectionBooks.js
@@ -1,4 +1,5 @@
-const imagesList = document.querySelectorAll(".image-book");
+const bookItems = document.querySelectorAll(".collection-books_list > li");
+
 async function bookImages() {
   try {
     const response = await fetch(
@@ -7,11 +8,27 @@ async function bookImages() {
     const data = await response.json();
     const bookList = data.results;
 
-    imagesList.forEach((imgTag, index) => {
+    bookItems.forEach((lisItem, index) => {
       if (bookList[index]) {
-        imgTag.src = bookList[index].capa;
-        imgTag.alt = bookList[index].titulo;
-      }
+        // 1. Capa
+        const imgTag = lisItem.querySelector(".image-book");
+        if (imgTag) {
+          imgTag.src = bookList[index].capa;
+          imgTag.alt = bookList[index].titulo;
+        }
+
+        // 2. Título
+        const titleTag = lisItem.querySelector("figcaption strong");
+        if (titleTag) {
+          titleTag.textContent = bookList[index].titulo;
+        }
+
+        // 3. Autor
+        const authorTag = lisItem.querySelector("figcaption p");
+        if (authorTag) {
+          authorTag.textContent = bookList[index].autor;
+        }
+      } 
     });
   } catch (error) {
     console.error("Error fetching data from API:", error);
